be7dca1b18450ceb57bd5d3c251b5e53694e997d
Gemi Yudhia <githuba122@gmail.com>
2026-07-20 22:56:51 +0700

    feat(users): handle user not found with NestJS exceptions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { NotFoundException } from '@nestjs/common';
+import { error } from 'console';
 
 @Injectable()
 export class UsersService {
@@ -21,19 +23,23 @@ export class UsersService {
     });
   }
 
-  findOne(id: number) {
-    return this.prisma.user.findUnique({
+  async findOne(id: number) {
+    const user = await this.prisma.user.findUnique({
       where: {
         id,
       },
       select: {
-        id: true,
         name: true,
+        id: true,
         email: true,
         createdAt: true,
         updatedAt: true,
       },
     });
+
+    if (!user) throw new NotFoundException(`User with ID ${id} not found`);
+
+    return user;
   }
 
   async create(createUserDto: CreateUserDto) {
